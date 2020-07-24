411675e249cd43d2698abff9e6449a2dc7c20416
pauchye <olga.a.smi@gmail.com>
2020-07-23 22:44:25 -0400

    Updates

---
+++ precache-manifest.887b686a1399ebad5ff26937dda0b544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045b2b1ef1dc67b27f6d8afdd36c5cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd3fd5c894d3528f33b",
+    "url": "/static/css/main.36044a45.chunk.css"
+  },
+  {
+    "revision": "dad96f200f92119dcc68",
+    "url": "/static/js/2.ec7045be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec7045be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd3fd5c894d3528f33b",
+    "url": "/static/js/main.0a695d68.chunk.js"
+  },
+  {
+    "revision": "fca20b0493a390a502f2",
+    "url": "/static/js/runtime-main.3e9cbd4c.js"
+  }
+]);
